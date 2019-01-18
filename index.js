646c038026701b0f2dab5f1b42309655c3b58049
梁娟 <liangjuan@mail.taiji.com.cn>
2019-01-18 17:18:41 +0800

    es6学习前5节

---
+++ index.js
@@ -0,0 +1,13 @@
+// let jspang='技术胖';
+// let blog = '非常高兴你能看到这篇文章，我是你的老朋友'+jspang+'。这节课我们学习字符串模版。';
+// document.write(blog);
+// let jspang='技术胖';
+// let blog = `非常高兴你能看到这篇文章，我是你的老朋友${jspang}这节课我们学习字符串模版。`;
+//字符计算
+// let a=1;
+// let b=2;
+// let result=`${a+b}`;
+// document.write(blog.includes(jspang));
+// document.write(blog.startsWith(jspang));
+//复制字符串
+document.write('*'.repeat(3));
